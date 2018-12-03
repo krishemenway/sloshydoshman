5df93579b4ab3a202a4877b127445637d3205496
Kris Hemenway <krishemenway@gmail.com>
2018-12-02 17:16:37 -0800

    adjusted size of difficulty column to fit "Hell on Earth" on more renderers

--- SloshyDoshMan.Web/src/HomeView/ServerRecentGamesComponent.ts
+++ SloshyDoshMan.Web/src/HomeView/ServerRecentGamesComponent.ts
@@ -65,8 +65,8 @@ ko.components.register(Name, {
 					<tbody>
 						<!-- ko foreach: LoadedGames -->
 						<tr class="clickable" style="line-height: 26px" data-bind="click: $component.SelectGame">
-							<td class="padding-half text-left" style="width: 70%"><!-- ko text: Map --><!-- /ko -->&nbsp;<span class="text-gray margin-left" data-bind="${MomentFormatDate.Name}: {Format: 'MMMM Do, YYYY', Date: TimeStarted}"></span></td>
-							<td class="padding-half text-center" style="width: 10%" data-bind="text: Difficulty"></td>
+							<td class="padding-half text-left" style="width: 69%"><!-- ko text: Map --><!-- /ko -->&nbsp;<span class="text-gray margin-left" data-bind="${MomentFormatDate.Name}: {Format: 'MMMM Do, YYYY', Date: TimeStarted}"></span></td>
+							<td class="padding-half text-center" style="width: 11%" data-bind="text: Difficulty"></td>
 							<td class="padding-half text-center" style="width: 10%" data-bind="text: Length"></td>
 							<td class="padding-half text-center" style="width: 10%"><!-- ko text: ReachedWave --><!-- /ko -->&nbsp;/&nbsp;<!-- ko text: TotalWaves --><!-- /ko --></td>
 						</tr>

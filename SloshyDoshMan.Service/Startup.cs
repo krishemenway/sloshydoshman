1c62d69d4d6772c62be0892b06d89f6de877527d
Kris Hemenway <krishemenway@gmail.com>
2019-11-17 23:35:55 -0800

    added missing routing setup on service

--- SloshyDoshMan.Service/Startup.cs
+++ SloshyDoshMan.Service/Startup.cs
@@ -23,6 +23,7 @@ namespace SloshyDoshMan.Service
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app)
 		{
+			app.UseRouting();
 			app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 		}
 

fda2d94e44798f6f33901a1100fdaab3b4b0114f
Kris Hemenway <krishemenway@gmail.com>
2020-05-20 01:13:53 -0700

    moving hosting of perk images to service

---
+++ SloshyDoshMan.Service/Perks/PerkImagesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace SloshyDoshMan.Service.Perks
+{
+	[Route("Perks")]
+	public class PerkImagesController : ControllerBase
+	{
+		public PerkImagesController(IMemoryCache memoryCache)
+		{
+			_memoryCache = memoryCache;
+		}
+
+		[HttpGet("{Perk}-128")]
+		public FileContentResult PerkImage128(string perk)
+		{
+			return File(ReadFileBytes($"PerksImages/{perk}-128.png"), "image/png");
+		}
+
+		private byte[] ReadFileBytes(string filePath)
+		{
+			return _memoryCache.GetOrCreate($"PerkImage128-{filePath}", (cache) => System.IO.File.ReadAllBytes(Program.Settings.FilePathInExecutableFolder(filePath)));
+		}
+
+		private readonly IMemoryCache _memoryCache;
+	}
+}
